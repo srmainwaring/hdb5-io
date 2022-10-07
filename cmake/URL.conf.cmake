28bf24556e1d49a9fc680578a7c816f4d26a07d3
Pierre-Yves Wuillaume <pierre-yves.wuillaume@dice-engineering.com>
2022-10-07 14:56:47 +0200

    Update Hdf5.

--- cmake/URL.conf.cmake
+++ cmake/URL.conf.cmake
@@ -9,6 +9,11 @@ set(MATHUTILS_BUILD_TESTS OFF CACHE BOOL "")
 set(MATHUTILS_BUILD_BOOST_TESTS OFF CACHE BOOL "")
 
 
+# HDF5
+set(hdf5_URL https://github.com/HDFGroup/hdf5/archive/refs/tags/hdf5-1_13_1.tar.gz)
+set(hdf5_TAG 1.13.1)
+
+
 # HighFive
 set(highfive_URL https://github.com/BlueBrain/HighFive.git)
 set(highfive_TAG v2.2.2)
@@ -23,13 +28,6 @@ set(googletest_TAG release-1.10.0 CACHE STRING "googletest version")
 # meshoui
 set(meshoui_URL git@frydom-ce.org:ce/meshoui.git)
 set(meshoui_TAG v1.3.1 CACHE STRING "meshoui version")
-
-
-# HDF5
-set(HDF5_URL https://github.com/HDFGroup/hdf5.git)
-set(HDF5_TAG hdf5-1_10_6 CACHE STRING "HDF5 version for building from scratch")
-set(HDF5_FIND_TAG 1.10 CACHE STRING "Minimal version of HDF5 to find on the system")
-set(HDF5_PATCH hdf5.patch)
 
 # VTK
 set(VTK_TAG 8.2 CACHE STRING "VTK version")
