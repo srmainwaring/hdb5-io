63d3ab03236f43547bcbd6d36aeb9137be1a0946
Pierre-Yves Wuillaume <pierre-yves.wuillaume@dice-engineering.com>
2022-10-07 14:59:10 +0200

    Use of FetchContent_MakeAvailable in HighFive.

--- cmake/Add_HighFive.cmake
+++ cmake/Add_HighFive.cmake
@@ -8,7 +8,7 @@ FetchContent_Declare(highfive
 FetchContent_GetProperties(highfive)
 if (NOT highfive_POPULATED)
     message(STATUS "******* FETCHING highfive dependency from ${PROJECT_NAME} (requested version: ${highfive_TAG}) *******")
-    FetchContent_Populate(highfive)
+    #FetchContent_Populate(highfive)
 
     # Highfive BUILD OPTIONS
     set(HIGHFIVE_USE_BOOST OFF CACHE BOOL "")
@@ -20,8 +20,10 @@ if (NOT highfive_POPULATED)
     set(HIGHFIVE_PARALLEL_HDF5 OFF CACHE BOOL "")
     set(HIGHFIVE_BUILD_DOCS OFF CACHE BOOL "")
 
-    add_library(HighFive INTERFACE)
-    target_compile_definitions(HighFive INTERFACE HIGHFIVE_USE_EIGEN=ON)
-    target_include_directories(HighFive INTERFACE ${highfive_SOURCE_DIR}/include)
+    FetchContent_MakeAvailable(highfive)
+
+#    add_library(HighFive INTERFACE)
+#    target_compile_definitions(HighFive INTERFACE HIGHFIVE_USE_EIGEN=ON)
+#    target_include_directories(HighFive INTERFACE ${highfive_SOURCE_DIR}/include)
 endif ()
 
